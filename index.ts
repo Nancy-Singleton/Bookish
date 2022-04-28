d03ef47b09e582e7fa8c61b4d75ec6e73fd82915
Nancy Singleton <Nancy.Singleton@softwire.com>
2022-04-28 16:13:28 +0100

    Add books endpoint

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+import {stringify} from "querystring";
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req: any, res: { send: (arg0: string) => void }) => {
+    res.send('Hello World!');
+})
+
+app.get('/books', (req: any, res: { send: (arg0: string) => void }) => {
+    const books = [{title: "dummy book 1"}, {title: "dummy book 2"}];
+    const booksString = JSON.stringify(books);
+    res.send(booksString);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
