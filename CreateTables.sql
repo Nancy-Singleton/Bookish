54f076016824cad3786ac7dad98b3eb0b0ebf7c1
Nancy Singleton <Nancy.Singleton@softwire.com>
2022-04-28 15:51:40 +0100

    Add CreateTables.sql

---
+++ CreateTables.sql
@@ -0,0 +1,40 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE Usr (
+ID   SERIAL       PRIMARY KEY NOT NULL,
+NAME VARCHAR(200)             NOT NULL
+);
+
+CREATE TABLE UsrSession (
+ID           SERIAL       PRIMARY KEY    NOT NULL,
+UsrID        INT          REFERENCES Usr NOT NULL,
+SessionToken VARCHAR(500)                NOT NULL,
+ExpiryDate   DATE                        NOT NULL
+);
+
+CREATE TABLE Book (
+ID    SERIAL       PRIMARY KEY NOT NULL,
+Title VARCHAR(500)             NOT NULL,
+ISBN  INT                      NOT NULL
+);
+
+CREATE TABLE BookCopy (
+ID      SERIAL PRIMARY KEY     NOT NULL,
+Barcode INT    UNIQUE          NOT NULL,
+BookID  INT    REFERENCES Book NOT NULL,
+UsrID   INT    REFERENCES Usr,
+DueDate DATE
+);
+
+CREATE TABLE Author (
+ID   SERIAL       PRIMARY KEY NOT NULL,
+NAME VARCHAR(200)             NOT NULL
+);
+
+CREATE TABLE BookAuthor (
+ID       SERIAL PRIMARY KEY       NOT NULL,
+BookID   INT    REFERENCES Book   NOT NULL,
+AuthorID INT    REFERENCES Author NOT NULL
+);
+
+COMMIT;
