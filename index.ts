201e65ea79539505ae4dde31391cfd69430bbe86
Nancy Singleton <Nancy.Singleton@softwire.com>
2022-04-28 16:39:14 +0100

    Allow /book endpoint to return books

--- index.ts
+++ index.ts
@@ -4,14 +4,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://bookish:dbpassword@localhost:5432/bookish')
+
+db.one('SELECT * FROM Book')
+    .then((data: any) => {
+        console.log('DATA:', data)
+    })
+    .catch((error: Error) => {
+        console.log('ERROR:', error)
+    })
+
 app.get('/', (req: any, res: { send: (arg0: string) => void }) => {
     res.send('Hello World!');
 })
 
 app.get('/books', (req: any, res: { send: (arg0: string) => void }) => {
-    const books = [{title: "dummy book 1"}, {title: "dummy book 2"}];
-    const booksString = JSON.stringify(books);
-    res.send(booksString);
+
+    db.one('SELECT * FROM Book')
+        .then((data: any) => {
+            console.log('DATA:', data)
+            const booksString = JSON.stringify(data);
+            res.send(booksString);
+        })
+        .catch((error: Error) => {
+            console.log('ERROR:', error)
+            res.send(error.message);
+        })
+
 })
 
 app.listen(port, () => {
